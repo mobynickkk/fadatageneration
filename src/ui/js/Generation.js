4aa7873ef9434d4c1558f25bf490385f43415921
Владимир <69643094+jay-dit@users.noreply.github.com>
2021-10-31 16:20:26 +0300

    Переназвал и отформатировал

--- src/ui/js/Generation.js
+++ src/ui/js/Generation.js
@@ -1,8 +1,8 @@
 
-$('document').ready(function(){
-	$('.generate').click(function(){
-		
-		
+$('document').ready(function () {
+	$('.generate').click(function () {
+
+
 		var hash = document.querySelector('.main-block').getAttribute('id')
 		var control = true
 		var main_dict = {}
@@ -10,35 +10,35 @@ $('document').ready(function(){
 		main_dict["user_id"] = `${hash}`
 		main_dict["function"] = []
 		var words = ['import', 'compile', 'eval', 'exec', 'try', 'except', 'return', 'def', 'class', 'type', 'dir', 'dict', 'list', 'tuple', 'while', 'for', ' in', 'range', 'yield', 'async', 'await', 'lambda']
-		document.querySelectorAll('.pole_for_func').forEach(function(el){
+		document.querySelectorAll('.pole_for_func').forEach(function (el) {
 			control = true
 			var id = el.getAttribute('id')
-			var F = String(el.querySelector('#F_{i}'.replace(new RegExp("{i}", "g"),id)).value)
-			for (v in words){
-				if (F.includes(words[v])){
+			var F = String(el.querySelector('#F_{i}'.replace(new RegExp("{i}", "g"), id)).value)
+			for (v in words) {
+				if (F.includes(words[v])) {
 					control = false
-					
+
 				}
-				
+
 			}
-	
-			var L = parseFloat(el.querySelector('#L_{i}'.replace(new RegExp("{i}", "g"),id)).value)
-			var R = parseFloat(el.querySelector('#R_{i}'.replace(new RegExp("{i}", "g"),id)).value)
-			if (L > R){control = false};
-			
-			var S = parseFloat(el.querySelector('#S_{i}'.replace(new RegExp("{i}", "g"),id)).value)
-			if (S > Math.abs(L-R)|| S < 0){control = false};
-			
-			var A = parseFloat(el.querySelector('#A_{i}'.replace(new RegExp("{i}", "g"),id)).value)
-			if (A <= 0 || A >= 1 ){control = false};
-			
+
+			var L = parseFloat(el.querySelector('#L_{i}'.replace(new RegExp("{i}", "g"), id)).value)
+			var R = parseFloat(el.querySelector('#R_{i}'.replace(new RegExp("{i}", "g"), id)).value)
+			if (L > R) { control = false };
+
+			var S = parseFloat(el.querySelector('#S_{i}'.replace(new RegExp("{i}", "g"), id)).value)
+			if (S > Math.abs(L - R) || S < 0) { control = false };
+
+			var A = parseFloat(el.querySelector('#A_{i}'.replace(new RegExp("{i}", "g"), id)).value)
+			if (A <= 0 || A >= 1) { control = false };
+
 			var V = el.querySelectorAll('.checkbox-input')[0].checked
 			var P = el.querySelectorAll('.checkbox-input')[1].checked
-			
-		
-			if (F == ""|| isNaN(L)|| isNaN(R)|| isNaN(S)|| isNaN(A)){control=false}
-			
-			if (control){
+
+
+			if (F == "" || isNaN(L) || isNaN(R) || isNaN(S) || isNaN(A)) { control = false }
+
+			if (control) {
 				help_dict = {}
 				help_dict['function'] = `${F}`
 				help_dict['range_from'] = `${L}`
@@ -48,14 +48,14 @@ $('document').ready(function(){
 				help_dict['use_emissions'] = `${V}`
 				help_dict['use_template'] = `${P}`
 				main_dict["function"].push(help_dict)
-			}else{
-				$('#{id}'.replace(new RegExp("{id}", "g"),id)).css('borderColor','red')
+			} else {
+				$('#{id}'.replace(new RegExp("{id}", "g"), id)).css('borderColor', 'red')
 			}
-			
-			})
-			
-		
-		
+
+		})
+
+
+
 		console.log(JSON.stringify(main_dict));
 		/* $.ajax({
 			type:"POST",
@@ -63,11 +63,10 @@ $('document').ready(function(){
 			data:JSON.stringify(main_dict),
 			success: function(data){$(".plot").attr("src", data);}
 		}) */
-		
-		})	
-		
-		
-		})
-		
-	
-		
+
+	})
+
+
+})
+
+
--- src/ui/js/hash.js
+++ src/ui/js/hash.js
@@ -1,14 +1,1 @@
-
-$( document ).ready(function(){
-String.prototype.hash = function() {
-  var self = this, range = Array(this.length);
-  for(var i = 0; i < this.length; i++) {
-    range[i] = i;
-  }
-  return Array.prototype.map.call(range, function(i) {
-    return self.charCodeAt(i).toString(16);
-  }).join('');
-};
-
-$('.main-block').attr('id', Date.now())
-});
+$(document).ready(function () {$('.main-block').attr('id', Date.now())});
