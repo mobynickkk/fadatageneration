6b0c41977f8a6459e6eec1b631d20a4225eea6f0
mobynick <mobynick365@gmail.com>
2021-11-05 21:42:07 +0300

    Фикс для гуникорна

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app
+web: gunicorn app:runtime_application
--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from test import MockMathService, MockValidationService
 
 
 class Application:
-    __app = Flask(__name__, template_folder='src/ui/', static_folder='src/ui/')
+    app = Flask(__name__, template_folder='src/ui/', static_folder='src/ui/')
     math_service: MathServiceInterface
     validation_service: ValidationServiceInterface
 
@@ -17,7 +17,7 @@ class Application:
         self.__static_init(self)
 
     def run(self, *args, **kwargs):
-        self.__app.run(*args, **kwargs)
+        self.app.run(*args, **kwargs)
 
     @staticmethod
     def __process_json(json_, class_):
@@ -30,23 +30,23 @@ class Application:
     @staticmethod
     def __static_init(instance):
 
-        @instance.__app.route('/')
+        @instance.app.route('/')
         def index():
             return render_template('index.html')
 
-        @instance.__app.route('/js/<path:path>')
+        @instance.app.route('/js/<path:path>')
         def send_js(path):
             return send_from_directory('src/ui/js', path)
 
-        @instance.__app.route('/img/<path:path>')
+        @instance.app.route('/img/<path:path>')
         def send_img(path):
             return send_from_directory('src/ui/img', path)
 
-        @instance.__app.route('/css/<path:path>')
+        @instance.app.route('/css/<path:path>')
         def send_css(path):
             return send_from_directory('src/ui/css', path)
 
-        @instance.__app.route('/api/', methods=['POST'])
+        @instance.app.route('/api/', methods=['POST'])
         def api():
             json_ = request.get_json()
             user_id: str = json_['user_id']
@@ -64,11 +64,8 @@ class Application:
                 return f'Что-то пошло не так\n\t{e}'
 
 
+runtime_application = Application(FunctionGraphicService(), FunctionValidationService()).app
+
 if __name__ == '__main__':
-    from sys import argv
-    if len(argv) >= 2 and argv[1] == 'test':
-        application = Application(MockMathService(), MockValidationService())
-        application.run(debug=True)
-    else:
-        application = Application(FunctionGraphicService(), FunctionValidationService())
-        application.run()
+    application = Application(MockMathService(), MockValidationService())
+    application.run(debug=True)
