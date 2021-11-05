e273df55d71e1a52f47ad77762eea99f2684615e
mobynick <mobynick365@gmail.com>
2021-11-05 19:54:08 +0300

    Рефакторинг FunctionGraphicService.run()

--- src/services/FunctionGraphicService.py
+++ src/services/FunctionGraphicService.py
@@ -1,38 +1,46 @@
 import random
+import numpy as np
+import matplotlib.pyplot as plt
 
 from .MathServiceInterface import MathServiceInterface
+from ..exceptions import CalculationError
 from ..dto import TaskDto, CompletedTaskDto
 
 
 class FunctionGraphicService(MathServiceInterface):
 
     @staticmethod
-    def step_h(range_from, range_to):
-        if 1 < abs(range_from - range_to):
-            r = random.random() / 10
-        else:
-            r = abs(range_from - range_to) / (random.random() * 100)
-        return r
+    def step_h(range_from: float, range_to: float) -> float:
+        return random.random() / 10 \
+            if 1 < abs(range_from - range_to) \
+            else abs(range_from - range_to) / (random.random() * 100)
+
+    @staticmethod
+    def __save_data(x: np.ndarray, y: np.ndarray, dto: TaskDto):
+        plt.scatter(x, y)
+        plt.savefig(f'{dto.user_id}.png', dpi=100)
+        np.savetxt(f'{dto.user_id}.csv', np.vstack((x, y)).T, delimiter=',')
+        return CompletedTaskDto(f'{dto.user_id}.csv', f'{dto.user_id}.png')
+
+    @staticmethod
+    def get_factor() -> float:
+        return (0.5 - random.random()) * 10
 
     def run(self, dto: TaskDto) -> CompletedTaskDto:
-        from numpy import arange, sin, cos, e, random, append, savetxt, vstack, delete
-        from matplotlib.pyplot import scatter, savefig
         from warnings import filterwarnings
 
         filterwarnings("ignore", category=FutureWarning)
 
         for func in dto.functions:
             if func.use_template:
-                func.function = func.function.replace('x', '({}*x+{})'.format((0.5 - random.random()) * 10,
-                                                                              (0.5 - random.random()) * 10))
+                func.function = func.function.replace('x', f'({self.get_factor()}*x+{self.get_factor()})')
                 func.function = func.function.replace(func.function,
-                                                      '{}*({})+{}'.format((0.5 - random.random()) * 10, func.function,
-                                                                          (0.5 - random.random()) * 10))
+                                                      f'{self.get_factor()}*({func.function})+{self.get_factor()}')
 
-        main_x = arange(dto.functions[0].range_from, dto.functions[0].range_to,
-                        dto.functions[0].step if dto.functions[0].step else FunctionGraphicService.step_h(
-                            dto.functions[0].range_from,
-                            dto.functions[0].range_to))
+        main_x = np.arange(dto.functions[0].range_from, dto.functions[0].range_to,
+                           dto.functions[0].step
+                           if dto.functions[0].step
+                           else self.step_h(dto.functions[0].range_from, dto.functions[0].range_to))
 
         if dto.functions[0].use_emissions:
             main_y = []
@@ -41,9 +49,9 @@ class FunctionGraphicService(MathServiceInterface):
                 try:
                     y = eval(dto.functions[0].function.replace('x', str(el)))
                     main_y.append(y + dto.functions[0].accuracy * (0.5 - random.random()))
-                except:
+                except Exception:
                     d.append(ind)
-            main_x = delete(main_x, d)
+            main_x = np.delete(main_x, d)
             del d
         else:
             main_y = []
@@ -52,33 +60,29 @@ class FunctionGraphicService(MathServiceInterface):
                 try:
                     y = eval(dto.functions[0].function.replace('x', str(el)))
                     main_y.append(y)
-                except:
+                except Exception:
                     d.append(ind)
-            main_x = delete(main_x, d)
+            main_x = np.delete(main_x, d)
             del d
 
         for func in dto.functions[1:]:
             c = main_y[-1] - eval(func.function.replace('x', str(func.range_from)))
+            step = func.step if func.step else self.step_h(func.range_from, func.range_to)
 
-            for i in arange(func.range_from, func.range_to,
-                            func.step if func.step else FunctionGraphicService.step_h(func.range_from, func.range_to)):
-
+            for i in np.arange(func.range_from, func.range_to, step):
                 if func.use_emissions:
                     try:
                         y = eval(func.function.replace('x', str(i))) + c
-                        main_x = append(main_x, i)
-                        main_y = append(main_y, y + func.accuracy * (0.5 - random.random()))
-                    except:
-                        pass
+                        main_x = np.append(main_x, i)
+                        main_y = np.append(main_y, y + func.accuracy * (0.5 - random.random()))
+                    except Exception:
+                        raise CalculationError()
                 else:
                     try:
                         y = eval(func.function.replace('x', str(i))) + c
-                        main_x = append(main_x, i)
-                        main_y = append(main_y, y)
-                    except:
-                        pass
+                        main_x = np.append(main_x, i)
+                        main_y = np.append(main_y, y)
+                    except Exception:
+                        raise CalculationError()
 
-        scatter(main_x, main_y)
-        savefig('{}.png'.format(dto.user_id), dpi=100)
-        savetxt('{}.csv'.format(dto.user_id), vstack((main_x, main_y)).T, delimiter=',')
-        return CompletedTaskDto('{}.csv'.format(dto.user_id), '{}.png'.format(dto.user_id))
+        return self.__save_data(main_x, main_y, dto)
